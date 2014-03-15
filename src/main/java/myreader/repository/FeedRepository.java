5cb0c8f8144d4e5f2704baf89ebe1068c4ae6d7d
Kamill Sokol <dev@sokol-web.de>
2014-03-15 17:22:28 +0100

    FeedRepository extends JpaRepository now

--- src/main/java/myreader/repository/FeedRepository.java
+++ src/main/java/myreader/repository/FeedRepository.java
@@ -1,13 +1,14 @@
 package myreader.repository;
 
 import myreader.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @author Kamill Sokol dev@sokol-web.de
  */
-public interface FeedRepository extends PagingAndSortingRepository<Feed, Long> {
+public interface FeedRepository extends JpaRepository<Feed, Long> {
 
     Feed findByUrl(String url);
 
