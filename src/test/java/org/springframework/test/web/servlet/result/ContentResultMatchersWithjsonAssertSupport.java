c2269437975698dfe6fa129d814a5af4b832e906
Kamill Sokol <dev@sokol-web.de>
2014-04-29 18:37:37 +0100

    [ci skip] generate html cobertura report

---
+++ src/test/java/org/springframework/test/web/servlet/result/ContentResultMatchersWithjsonAssertSupport.java
@@ -0,0 +1,26 @@
+package org.springframework.test.web.servlet.result;
+
+import org.springframework.test.web.servlet.JsonUtils;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.io.UnsupportedEncodingException;
+
+import static net.javacrumbs.jsonunit.JsonAssert.assertJsonEquals;
+
+/**
+ * @author Kamill Sokol
+ */
+public class ContentResultMatchersWithjsonAssertSupport {
+
+    public static ResultMatcher isJsonEqual(final String pathToJsonFile) {
+        return new ResultMatcher() {
+            public void match(MvcResult result) throws UnsupportedEncodingException {
+                String actual = result.getResponse().getContentAsString();
+                String json = JsonUtils.jsonFromFile(pathToJsonFile);
+                assertJsonEquals(json,  actual);
+            }
+        };
+    }
+
+}
