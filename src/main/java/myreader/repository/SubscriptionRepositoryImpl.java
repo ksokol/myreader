f4e8c2855bf5d3e6634175a2e289cf9a8f39e7ed
Kamill Sokol <dev@sokol-web.de>
2014-05-11 13:33:57 +0200

    fixed database and data type incompatibilities in SubscriptionRepositoryImpl

--- src/main/java/myreader/repository/SubscriptionRepositoryImpl.java
+++ src/main/java/myreader/repository/SubscriptionRepositoryImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,9 +20,9 @@ import java.util.List;
 @Repository
 class SubscriptionRepositoryImpl implements SubscriptionRepositoryCustom {
 
-    private static String SQL = "SELECT user_feed_tag, cast(SUM(user_feed_unseen) as INT), null "+
+    private static String SQL = "SELECT user_feed_tag, SUM(user_feed_unseen), null "+
                                 "FROM user_feed WHERE user_feed_user_id = ?1 AND user_feed_tag IS NOT NULL GROUP BY user_feed_tag "+
-                                "UNION SELECT user_feed_title, user_feed_unseen, CAST(user_feed_id as INT) "+
+                                "UNION SELECT user_feed_title, user_feed_unseen, user_feed_id "+
                                 "FROM user_feed WHERE user_feed_user_id = ?1 AND user_feed_tag IS NULL";
 
     private static String COUNT_SQL = "select sum(t.c1) from ( " +
@@ -43,8 +44,8 @@ class SubscriptionRepositoryImpl implements SubscriptionRepositoryCustom {
     private long getCount(Long userId) {
         Query nativeQuery = em.createNativeQuery(COUNT_SQL);
         nativeQuery.setParameter(1, userId);
-        List<Number> counts = (List<Number>) nativeQuery.getResultList();
-        return counts.get(0).longValue();
+        Number count = (Number) nativeQuery.getSingleResult();
+        return count.longValue();
     }
 
     private List<Object[]> getResultList(Pageable pageable, Long userId) {
@@ -64,15 +65,15 @@ class SubscriptionRepositoryImpl implements SubscriptionRepositoryCustom {
         ArrayList<TagGroup> tagGroups = new ArrayList<>();
         for (Object[] objects : resultList) {
             String name = (String) objects[0];
-            int id = 0;
-            int sum = (int) objects[1];
+            Number id = BigInteger.ZERO;
+            Number sum = (Number) objects[1];
             TagGroup.Type type = TagGroup.Type.AGGREGATE;
 
             if(objects[2] != null) {
-                id = (int) objects[2];
+                id = (Number) objects[2];
                 type = TagGroup.Type.SUBSCRIPTION;
             }
-            tagGroups.add(new TagGroup(id, name, sum, type));
+            tagGroups.add(new TagGroup(id.intValue(), name, sum.intValue(), type));
         }
         return tagGroups;
     }
