a6fae3376941cdcba0d4d7c2daa1b0031d9a6357
Kamill Sokol <dev@sokol-web.de>
2014-07-17 07:23:39 +0200

    inlined StringURLDecoderEditor

--- src/main/java/myreader/resource/config/CustomDelegatingWebMvcConfiguration.java
+++ src/main/java/myreader/resource/config/CustomDelegatingWebMvcConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
+import java.beans.PropertyEditorSupport;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 /**
  * @author Kamill Sokol
  */
@@ -27,7 +31,23 @@ class CustomDelegatingWebMvcConfiguration extends DelegatingWebMvcConfiguration 
         configurableWebBindingInitializer.setPropertyEditorRegistrar(new PropertyEditorRegistrar() {
             @Override
             public void registerCustomEditors(PropertyEditorRegistry registry) {
-                registry.registerCustomEditor(String.class, new StringURLDecoderEditor());
+                registry.registerCustomEditor(String.class, new PropertyEditorSupport() {
+                    @Override
+                    public void setAsText(String text) {
+                        if (text == null) {
+                            setValue(null);
+                        } else {
+                            String value = text.trim();
+
+                            try {
+                                String decoded = URLDecoder.decode(value, "UTF-8");
+                                setValue(decoded);
+                            } catch (UnsupportedEncodingException e) {
+                                throw new IllegalArgumentException(e.getMessage(), e);
+                            }
+                        }
+                    }
+                });
             }
         });
 
