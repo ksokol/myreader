3a896ac3225715225aba122a56f645e2869aa1a8
Kamill Sokol <dev@sokol-web.de>
2014-11-26 07:43:13 +0100

    unified validation error response and general error response

--- src/main/java/myreader/resource/exception/handler/Error.java
+++ src/main/java/myreader/resource/exception/handler/Error.java
@@ -1,11 +1,15 @@
 package myreader.resource.exception.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Kamill Sokol
  */
 class Error {
     private final int status;
     private final String message;
+    private List<FieldError> fieldErrors = new ArrayList<>();
 
     Error(int status, String message) {
         this.status = status;
@@ -18,5 +22,14 @@ class Error {
 
     public String getMessage() {
         return message;
+    }
+
+    public void addFieldError(String path, String message) {
+        FieldError error = new FieldError(path, message);
+        fieldErrors.add(error);
+    }
+
+    public List<FieldError> getFieldErrors() {
+        return fieldErrors;
     }
 }
--- src/main/java/myreader/resource/exception/handler/MethodArgumentNotValidExceptionHandler.java
+++ src/main/java/myreader/resource/exception/handler/MethodArgumentNotValidExceptionHandler.java
@@ -1,17 +1,17 @@
 package myreader.resource.exception.handler;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 /**
  * Kudos http://www.petrikainulainen.net/programming/spring-framework/spring-from-the-trenches-adding-validation-to-a-rest-api/
@@ -22,53 +22,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 class MethodArgumentNotValidExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ValidationErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public Error methodArgumentNotValidException(MethodArgumentNotValidException ex) {
         BindingResult result = ex.getBindingResult();
         List<org.springframework.validation.FieldError> fieldErrors = result.getFieldErrors();
         return processFieldErrors(fieldErrors);
     }
 
-    private ValidationErrorResponse processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
-        ValidationErrorResponse dto = new ValidationErrorResponse();
+    private Error processFieldErrors(List<org.springframework.validation.FieldError> fieldErrors) {
+        Error error = new Error(BAD_REQUEST.value(), "validation error");
         for (org.springframework.validation.FieldError fieldError: fieldErrors) {
-            dto.addFieldError(fieldError.getField(), fieldError.getDefaultMessage());
+            error.addFieldError(fieldError.getField(), fieldError.getDefaultMessage());
         }
-        return dto;
+        return error;
     }
 
-    class ValidationErrorResponse {
-
-        private List<FieldError> fieldErrors = new ArrayList<FieldError>();
-
-        public void addFieldError(String path, String message) {
-            FieldError error = new FieldError(path, message);
-            fieldErrors.add(error);
-        }
-
-        public List<FieldError> getFieldErrors() {
-            return fieldErrors;
-        }
-    }
-
-    class FieldError {
-
-        private String field;
-        private String message;
-
-        public FieldError(String field, String message) {
-            this.field = field;
-            this.message = message;
-        }
-
-        public String getField() {
-            return field;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
 }
