6806c8634d5f6165ec2b89d64159832f0c103fff
Kamill Sokol <dev@sokol-web.de>
2014-12-28 15:47:51 +0100

    fixed initd script

--- src/main/scripts/myreader.sh
+++ src/main/scripts/myreader.sh
@@ -19,10 +19,11 @@
 # This should be placed in /etc/init.d
 ### END INIT INFO
 
+MYREADER_NAME=myreader
 MYREADER_HOME=/opt/myreader
 MYREADER_CONF=${MYREADER_HOME}/config/application.properties
 MYREADER_USER=myreader
-MYREADER_PORT=9430
+MYREADER_PORT=9340
 
 if [ ! -e /etc/redhat-release ]; then
     echo "This is built for RHEL/Fedora/CentOS, you may need to alter it to fit your distribution"
@@ -30,31 +31,34 @@ if [ ! -e /etc/redhat-release ]; then
 fi
 :
 if [ -f ${MYREADER_CONF} ]; then
-    MYREADER_PORT=$(egrep -o "server.port=.*" ${MYREADER_HOME}/config/application.properties | cut -b 13-19)
+    TMP=$(egrep -o "server.port=.*" ${MYREADER_HOME}/config/application.properties | cut -b 13-19)
+    if [ ! -z ${TMP} ]; then
+        MYREADER_PORT=${TMP}
+    fi
 fi
 
 . /etc/rc.d/init.d/functions
 
 case $1 in
 start)
-    PID=$(lsof -i:${MYREADER_PORT} -t)
+    PID=$(lsof -i:${MYREADER_PORT} -a -u ${MYREADER_USER} -t)
 
     if [ ${PID} ]; then
-        echo "Already running ${NAME}" && warning
+        echo "Already running ${MYREADER_NAME}" && warning
         echo
         exit 1
     fi
 
-    action "Starting ${NAME}: " daemon --check myreader --user ${MYREADER_USER} "cd ${MYREADER_HOME}; nohup java -server -jar myreader.jar > ${MYREADER_HOME}/logs/nohup.out 2>&1 &"
+    action "Starting ${MYREADER_NAME}: " daemon --check myreader --user ${MYREADER_USER} "cd ${MYREADER_HOME}; nohup java -server -jar myreader.jar > ${MYREADER_HOME}/logs/nohup.out 2>&1 &"
     echo
 ;;
 stop)
-    PID=$(lsof -i:${MYREADER_PORT} -t)
+    PID=$(lsof -i:${MYREADER_PORT} -a -u ${MYREADER_USER} -t)
 
     if [ ${PID} ]; then
         action "Shutting down ${NAME}: " kill ${PID}
     else
-        echo -n "Service ${NAME} not running!" && failure
+        echo -n "Service ${MYREADER_NAME} not running!" && failure
         echo
     fi
 
@@ -65,12 +69,12 @@ restart)
     $0 start
 ;;
 status)
-    PID=$(lsof -i:${MYREADER_PORT} -t | sed -n 1p)
+    PID=$(lsof -i:${MYREADER_PORT} -a -u ${MYREADER_USER} -t)
 
     if [ ${PID} ]; then
-        echo "${NAME} running PID: ${PID}"
+        echo "${MYREADER_NAME} running PID: ${PID}"
     else
-        echo "${NAME} not running"
+        echo "${MYREADER_NAME} not running"
     fi
 ;;
 *)
