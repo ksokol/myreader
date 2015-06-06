3c9db03e05d704a1e742d84474012beb3c00a9b3
Kamill Sokol <dev@sokol-web.de>
2015-06-06 23:57:15 +0200

    angular-cache release 4.2.0

--- src/main/java/myreader/config/jawr/JavascriptConfigPropertiesSource.java
+++ src/main/java/myreader/config/jawr/JavascriptConfigPropertiesSource.java
@@ -30,7 +30,7 @@ public class JavascriptConfigPropertiesSource {
                     .webjar("ui-router-extras/0.0.13/modular/ct-ui-router-extras.transition.js")
                     .webjar("ui-router-extras/0.0.13/modular/ct-ui-router-extras.previous.js")
                     .webjar("angular-local-storage/0.1.5/angular-local-storage.js")
-                    .jar("static/app/js/lib/angular-cache.js") //TODO submit newest version to webjars
+                    .webjar("angular-cache/4.2.0/angular-cache.js") //TODO submit newest version to webjars
                     .webjar("angular-material/0.9.7/angular-material.js")
                     .jar("static/app/js/common/config.js")
                     .jar("static/app/js/common/api.js")
