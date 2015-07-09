7c8f2d4fbea7c20e15e62c17af9e4fbd60f1db56
Kamill Sokol <dev@sokol-web.de>
2015-07-09 21:21:41 +0200

    fixed ExclusionSetCollectionResourceTest

--- src/test/java/myreader/resource/exclusionset/ExclusionSetCollectionResourceTest.java
+++ src/test/java/myreader/resource/exclusionset/ExclusionSetCollectionResourceTest.java
@@ -1,13 +1,13 @@
 package myreader.resource.exclusionset;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser1;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser2;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser3;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser4;
 import static org.springframework.test.web.servlet.result.ContentResultMatchersJsonAssertSupport.jsonEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-
 import myreader.test.IntegrationTestSupport;
+
+import org.junit.Test;
 
 /**
  * @author Kamill Sokol
@@ -16,15 +16,15 @@ public class ExclusionSetCollectionResourceTest extends IntegrationTestSupport {
 
     @Test
     public void testCollectionResourceForUser1JsonStructureEquality() throws Exception {
-        mockMvc.perform(getAsUser1("/exclusions"))
+        mockMvc.perform(getAsUser3("/exclusions"))
                 .andExpect(status().isOk())
-                .andExpect(jsonEquals("json/exclusionset/user1.json"));
+                .andExpect(jsonEquals("json/exclusionset/user3.json"));
     }
 
     @Test
     public void testCollectionResourceForUser2JsonStructureEquality() throws Exception {
-        mockMvc.perform(getAsUser2("/exclusions"))
+        mockMvc.perform(getAsUser4("/exclusions"))
                 .andExpect(status().isOk())
-                .andExpect(jsonEquals("json/exclusionset/user2.json"));
+                .andExpect(jsonEquals("json/exclusionset/user4.json"));
     }
 }
