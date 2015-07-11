d88fee97d085da2e500be7afc8a5cd4b6439a217
Kamill Sokol <dev@sokol-web.de>
2015-07-11 20:03:22 +0200

    added missing code coverage for XAuthoritiesFilter

---
+++ src/test/java/spring/security/XAuthoritiesFilterTests.java
@@ -0,0 +1,42 @@
+package spring.security;
+
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+import java.security.Principal;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Kamill Sokol
+ */
+public class XAuthoritiesFilterTests {
+
+    private static final String X_MY_AUTHORITIES = "X-MY-AUTHORITIES";
+    private XAuthoritiesFilter filter = new XAuthoritiesFilter();
+
+    @Test
+    public void noPrincipal() throws Exception {
+        final HttpServletResponse mockHttpServletResponse = setup(null);
+        assertThat(mockHttpServletResponse.getHeader(X_MY_AUTHORITIES), nullValue());
+    }
+
+    @Test
+    public void principalWithoutAuthorities() throws Exception {
+        final HttpServletResponse mockHttpServletResponse = setup(new UsernamePasswordAuthenticationToken("irrelevant", "irrelevant"));
+        assertThat(mockHttpServletResponse.getHeader(X_MY_AUTHORITIES), nullValue());
+    }
+
+    private HttpServletResponse setup(Principal principal) throws Exception {
+        final MockHttpServletRequest mockHttpServletRequest = new MockHttpServletRequest();
+        mockHttpServletRequest.setUserPrincipal(principal);
+        final MockHttpServletResponse mockHttpServletResponse = new MockHttpServletResponse();
+        filter.doFilter(mockHttpServletRequest, mockHttpServletResponse, new MockFilterChain());
+        return mockHttpServletResponse;
+    }
+}
