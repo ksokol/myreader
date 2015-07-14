1e35d8d060efd8913aee2b582e2a2c1371dc7cdf
Kamill Sokol <dev@sokol-web.de>
2015-07-14 22:09:48 +0200

    added missing code coverage for EntryLinkSanitizer

--- src/main/java/myreader/fetcher/impl/EntryLinkSanitizer.java
+++ src/main/java/myreader/fetcher/impl/EntryLinkSanitizer.java
@@ -1,5 +1,7 @@
 package myreader.fetcher.impl;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -9,37 +11,36 @@ import java.util.regex.Pattern;
  * @author Kamill Sokol
  */
 public final class EntryLinkSanitizer {
-	private EntryLinkSanitizer() {}
 
-	private static final Pattern pattern = Pattern.compile("^http(s)?://.*");
+    private static final Pattern PATTERN = Pattern.compile("^http(s)?://.*");
 
-    public static String sanitize(String entryLink, String feedLink) {
-        return sanitize(entryLink, feedLink, null);
+    public static String sanitize(String entryLink, String feedLink, String fetchUrl) {
+        Assert.notNull(fetchUrl, "fetchUrl is null");
+        if (StringUtils.isBlank(entryLink) || StringUtils.isBlank(feedLink)) {
+            return StringUtils.EMPTY;
+        }
+
+        entryLink = entryLink.trim();
+
+        if (PATTERN.matcher(entryLink).matches()) {
+            return entryLink;
+        }
+
+        String baseUrl = feedLink.trim();
+
+        if (!PATTERN.matcher(feedLink).matches()){
+            UriComponents uriComponents = UriComponentsBuilder.fromUriString(fetchUrl).build();
+            String scheme = uriComponents.getScheme();
+            String host = uriComponents.getHost();
+            baseUrl = String.format("%s://%s", scheme, host);
+        }
+
+        String sep = StringUtils.EMPTY;
+
+        if(!entryLink.startsWith("/")) {
+            sep = "/";
+        }
+
+        return String.format("%s%s%s", baseUrl, sep, entryLink);
     }
-
-	public static String sanitize(String entryLink, String feedLink, String fetchUrl) {
-		if(entryLink == null || feedLink == null) {
-			return "";
-		}
-
-		entryLink = entryLink.trim();
-
-		if(pattern.matcher(entryLink).matches()) {
-			return entryLink;
-		}
-
-		String baseUrl = feedLink.trim();
-
-		if(fetchUrl != null && !pattern.matcher(feedLink).matches()) {
-			UriComponents uriComponents = UriComponentsBuilder.fromUriString(fetchUrl).build();
-			String scheme = uriComponents.getScheme();
-			String host = uriComponents.getHost();
-			baseUrl = String.format("%s://%s", scheme, host);
-		}
-
-		baseUrl = baseUrl.trim();
-		String sep = (entryLink.startsWith("/") || baseUrl.startsWith("/")) ? "" : "/";
-
-		return String.format("%s%s%s", baseUrl, sep, entryLink);
-	}
 }
