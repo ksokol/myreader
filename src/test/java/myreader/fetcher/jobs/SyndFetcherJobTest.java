9bc1782c85045a359dd0ed83bf63c68e1de6cf68
Kamill Sokol <dev@sokol-web.de>
2015-07-14 20:22:02 +0200

    added missing code coverage for SyndFetcherJob

--- src/test/java/myreader/fetcher/jobs/SyndFetcherJobTest.java
+++ src/test/java/myreader/fetcher/jobs/SyndFetcherJobTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -11,17 +12,25 @@ import static org.mockito.Mockito.when;
 import myreader.fetcher.FeedQueue;
 import myreader.fetcher.SubscriptionBatch;
 
+import myreader.test.IntegrationTestSupport;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
 
 /**
  * @author Kamill Sokol
  */
-public class SyndFetcherJobTest {
+public class SyndFetcherJobTest extends IntegrationTestSupport {
 
     private SyndFetcherJob uut;
     private FeedQueue queueMock;
     private SubscriptionBatch serviceMock;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
 
     @Before
     public void setUp() throws Exception {
@@ -43,5 +52,16 @@ public class SyndFetcherJobTest {
 
         verify(queueMock, times(3)).poll();
         verify(serviceMock, times(2)).updateUserSubscriptions(anyString());
+    }
+
+    @Test
+    public void shouldNeverCallService() throws Exception {
+        when(queueMock.poll()).thenReturn("url1", "url2", null);
+
+        uut.onApplicationEvent(new ContextClosedEvent(applicationContext));
+        uut.run();
+
+        verify(queueMock, times(1)).poll();
+        verify(serviceMock, never()).updateUserSubscriptions(anyString());
     }
 }
