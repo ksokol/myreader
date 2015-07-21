62f909d45e0491c5d295f3dbb308e72621644d30
Kamill Sokol <dev@sokol-web.de>
2015-07-21 05:42:50 +0200

    code coverage FeedListFetcherJob

--- src/test/java/myreader/fetcher/jobs/FeedListFetcherJobTests.java
+++ src/test/java/myreader/fetcher/jobs/FeedListFetcherJobTests.java
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import myreader.entity.Feed;
+import myreader.fetcher.FeedParseException;
 import myreader.fetcher.FeedParser;
 import myreader.fetcher.FeedQueue;
 import myreader.fetcher.persistence.FetchResult;
@@ -63,13 +64,16 @@ public class FeedListFetcherJobTests {
     }
 
     @Test
-    public void whenDecideReturnsFalseThenNeverAddToQueue() throws Exception {
+    public void whenReturnsNoResultThenNeverAddToQueue() throws Exception {
         final Feed feed = new Feed();
         feed.setUrl(URL);
         feed.setLastModified(LAST_MODIFIED);
 
+        final FetchResult fetchResult = new FetchResult(URL);
+
         when(queueMock.getSize()).thenReturn(0);
         when(feedRepositoryMock.findAll()).thenReturn(Collections.singletonList(feed));
+        when(feedParser.parse(URL, LAST_MODIFIED)).thenReturn(fetchResult);
 
         uut.run();
 
@@ -77,22 +81,39 @@ public class FeedListFetcherJobTests {
     }
 
     @Test
-    public void whenDecideReturnsTrueThenAddToQueue() throws Exception {
+    public void whenReturnsResultThenAddToQueue() throws Exception {
         final Feed feed = new Feed();
         feed.setUrl(URL);
         feed.setLastModified(LAST_MODIFIED);
 
-        when(queueMock.getSize()).thenReturn(0);
-        when(feedRepositoryMock.findAll()).thenReturn(Collections.singletonList(feed));
-
         final FetchResult fetchResult = new FetchResult(Arrays.asList(new FetcherEntry()), null, null);
         fetchResult.setUrl(URL);
 
+        when(queueMock.getSize()).thenReturn(0);
+        when(feedRepositoryMock.findAll()).thenReturn(Collections.singletonList(feed));
         when(feedParser.parse(URL, LAST_MODIFIED)).thenReturn(fetchResult);
 
         uut.run();
 
         verify(feedParser, times(1)).parse(URL, LAST_MODIFIED);
+        verify(queueMock, times(1)).add(fetchResult);
+    }
+
+    @Test
+    public void whenReturnsExceptionThenAddOneToQueue() throws Exception {
+        Feed feedMock1 = mock(Feed.class);
+        Feed feedMock2 = mock(Feed.class);
+
+        final FetchResult fetchResult = new FetchResult(Arrays.asList(new FetcherEntry()), null, null);
+        fetchResult.setUrl(URL);
+
+        when(queueMock.getSize()).thenReturn(0);
+        when(feedRepositoryMock.findAll()).thenReturn(Arrays.asList(feedMock1, feedMock2));
+        when(feedParser.parse(anyString(), anyString())).thenThrow(new FeedParseException()).thenReturn(fetchResult);
+
+        uut.run();
+
+        verify(feedParser, times(2)).parse(anyString(), anyString());
         verify(queueMock, times(1)).add(fetchResult);
     }
 }
