c9323100dd2a159f2a746707e91af207a5dd675e
Kamill Sokol <dev@sokol-web.de>
2015-07-30 20:53:31 +0200

    removed @DirtiesContext from StarterTest

--- src/test/java/myreader/StarterTest.java
+++ src/test/java/myreader/StarterTest.java
@@ -5,12 +5,10 @@ import static org.junit.Assert.fail;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.springframework.test.annotation.DirtiesContext;
 
 /**
  * @author Kamill Sokol
  */
-@DirtiesContext
 public class StarterTest {
 
     @Rule
