2f23c3f89dc72d4e7bfc3413ed273e092ed0a3b9
Kamill Sokol <dev@sokol-web.de>
2015-08-12 19:55:24 +0200

    fixed ExclusionPatternEntityResourceTest::testEntityResourceForUser1JsonStructureEquality

--- src/test/java/myreader/resource/exclusionpattern/ExclusionPatternEntityResourceTest.java
+++ src/test/java/myreader/resource/exclusionpattern/ExclusionPatternEntityResourceTest.java
@@ -1,5 +1,8 @@
 package myreader.resource.exclusionpattern;
 
+import static myreader.test.KnownUser.USER113;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.actionAsUserX;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.deleteAsUser1;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser1;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.getAsUser2;
@@ -16,9 +19,9 @@ public class ExclusionPatternEntityResourceTest extends IntegrationTestSupport {
 
     @Test
     public void testEntityResourceForUser1JsonStructureEquality() throws Exception {
-        mockMvc.perform(getAsUser1("/api/2/exclusions/1/pattern/0"))
+        mockMvc.perform(actionAsUserX(GET, USER113, "/api/2/exclusions/1101/pattern/7"))
                 .andExpect(status().isOk())
-                .andExpect(jsonEquals("json/exclusionpattern/1#pattern#0.json"));
+                .andExpect(jsonEquals("json/exclusionpattern/1101#pattern#7.json"));
     }
 
     @Test
