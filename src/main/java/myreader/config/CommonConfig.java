c0ee3d973bce612196f4e0cebaa914ca20eacbf2
Kamill Sokol <dev@sokol-web.de>
2015-08-22 15:01:15 +0200

    removed Jetty in favor of Tomcat. removed custom tmp dir

--- src/main/java/myreader/config/CommonConfig.java
+++ src/main/java/myreader/config/CommonConfig.java
@@ -1,21 +1,13 @@
 package myreader.config;
 
-import org.eclipse.jetty.server.session.HashSessionManager;
-import org.eclipse.jetty.server.session.SessionHandler;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import javax.servlet.ServletContext;
 
 /**
  * @author Kamill Sokol
@@ -38,23 +30,5 @@ public class CommonConfig {
     @Bean
     public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
         return new SecurityEvaluationContextExtension();
-    }
-    @Bean
-    public JettyEmbeddedServletContainerFactory jettyEmbeddedServletContainerFactory(@Value("${tmpdir:tmp}") final String tmpDir, @Value("${saveperiod:60}") final int savePeriod) {
-        return new JettyEmbeddedServletContainerFactory() {
-            @Override
-            protected void postProcessWebAppContext(final WebAppContext webAppContext) {
-                webAppContext.setAttribute(ServletContext.TEMPDIR, new File(tmpDir, "jawr")); //create tmp in current working directory
-
-                final HashSessionManager hashSessionManager = new HashSessionManager();
-                hashSessionManager.setSavePeriod(savePeriod);
-                try {
-                    hashSessionManager.setStoreDirectory(new File(tmpDir, "sessions"));
-                } catch (IOException e) {
-                    throw new RuntimeException(e.getMessage(), e);
-                }
-                webAppContext.setSessionHandler(new SessionHandler(hashSessionManager));
-            }
-        };
     }
 }
