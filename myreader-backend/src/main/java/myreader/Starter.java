d9c50b32a05e39a249eeb72383e195a2d5f93e5a
Kamill Sokol <dev@sokol-web.de>
2016-08-13 13:59:08 +0200

    Added proper test case to StarterTest. Added proper application context shutdown in StarterTest

--- myreader-backend/src/main/java/myreader/Starter.java
+++ myreader-backend/src/main/java/myreader/Starter.java
@@ -2,6 +2,7 @@ package myreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import java.util.TimeZone;
 
@@ -11,10 +12,12 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class Starter {
 
+    static ApplicationContext applicationContext;
+
     public static void main(String[] args) throws Exception {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC")); // required by hsqldb
-        System.setProperty("file.encoding","UTF-8");
-        SpringApplication.run(Starter.class, args);
+        System.setProperty("file.encoding", "UTF-8");
+        applicationContext = SpringApplication.run(Starter.class, args);
     }
 
 }
