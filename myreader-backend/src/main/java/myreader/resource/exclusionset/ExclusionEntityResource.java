08bc256e169b8843b4838ec0230cc05ecd25a068
Kamill Sokol <dev@sokol-web.de>
2017-01-19 22:02:01 +0100

    Replace custom ResourceAssemblers with Spring HATEOAS in ExclusionSetCollectionResource and ExclusionEntityResource

--- myreader-backend/src/main/java/myreader/resource/exclusionset/ExclusionEntityResource.java
+++ myreader-backend/src/main/java/myreader/resource/exclusionset/ExclusionEntityResource.java
@@ -5,12 +5,12 @@ import myreader.repository.ExclusionRepository;
 import myreader.resource.exception.ResourceNotFoundException;
 import myreader.resource.exclusionset.beans.ExclusionSetGetResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.mvc.ResourceAssemblerSupport;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import spring.hateoas.ResourceAssemblers;
 
 /**
  * @author Kamill Sokol
@@ -19,12 +19,13 @@ import spring.hateoas.ResourceAssemblers;
 @RequestMapping(value = "api/2/exclusions")
 public class ExclusionEntityResource {
 
-    private final ResourceAssemblers resourceAssemblers;
+    private final ResourceAssemblerSupport<ExclusionSet, ExclusionSetGetResponse> assembler;
     private final ExclusionRepository exclusionRepository;
 
     @Autowired
-    public ExclusionEntityResource(final ResourceAssemblers resourceAssemblers, final ExclusionRepository exclusionRepository) {
-        this.resourceAssemblers = resourceAssemblers;
+    public ExclusionEntityResource(final ResourceAssemblerSupport<ExclusionSet, ExclusionSetGetResponse> assembler,
+                                   final ExclusionRepository exclusionRepository) {
+        this.assembler = assembler;
         this.exclusionRepository = exclusionRepository;
     }
 
@@ -39,6 +40,6 @@ public class ExclusionEntityResource {
 
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
     public ExclusionSetGetResponse get(@ModelAttribute("exclusionSet") ExclusionSet exclusionSet) {
-        return resourceAssemblers.toResource(exclusionSet, ExclusionSetGetResponse.class);
+        return assembler.toResource(exclusionSet);
     }
 }
