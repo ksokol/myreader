8aadbd285f11c625fbd9cd20989a9ba9797faeef
Kamill Sokol <dev@sokol-web.de>
2017-01-20 22:35:01 +0100

    Remove unused PatchService

--- myreader-backend/src/main/java/myreader/resource/service/patch/PatchService.java
+++
@@ -1,9 +0,0 @@
-package myreader.resource.service.patch;
-
-/**
- * @author Kamill Sokol dev@sokol-web.de
- */
-public interface PatchService {
-
-    <T> T patch(PatchSupport patchable, T toPatch);
-}
