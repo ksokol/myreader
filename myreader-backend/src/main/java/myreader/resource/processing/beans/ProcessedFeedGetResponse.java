84fa683d4181df74a63ab1a0710912f407381579
Kamill Sokol <dev@sokol-web.de>
2017-01-20 19:00:16 +0100

    Replace UUIDResourceSupport with ResourceSupport in ProcessedFeedGetResponse

--- myreader-backend/src/main/java/myreader/resource/processing/beans/ProcessedFeedGetResponse.java
+++ myreader-backend/src/main/java/myreader/resource/processing/beans/ProcessedFeedGetResponse.java
@@ -1,19 +1,28 @@
 package myreader.resource.processing.beans;
 
-import spring.hateoas.UUIDResourceSupport;
+import org.springframework.hateoas.ResourceSupport;
 
 import java.util.Date;
 
 /**
  * @author Kamill Sokol
  */
-public class ProcessedFeedGetResponse extends UUIDResourceSupport {
+public class ProcessedFeedGetResponse extends ResourceSupport {
 
+    private String uuid;
     private String title;
     private String origin;
     private String lastModified;
     private Integer fetched;
     private Date createdAt;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
 
     public String getTitle() {
         return title;
@@ -53,25 +62,5 @@ public class ProcessedFeedGetResponse extends UUIDResourceSupport {
 
     public void setCreatedAt(final Date createdAt) {
         this.createdAt = new Date(createdAt.getTime());
-    }
-
-    @Override
-    public int hashCode() {
-        return getUuid().hashCode();
-    }
-
-    @Override
-    public boolean equals(final Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (object == null || !object.getClass().equals(this.getClass())) {
-            return false;
-        }
-
-        ProcessedFeedGetResponse that = (ProcessedFeedGetResponse) object;
-
-        return this.getUuid().equals(that.getUuid());
     }
 }
