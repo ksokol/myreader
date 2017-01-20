3b3c8a28d2fb0447e56beae277ee3fccd86a5440
Kamill Sokol <dev@sokol-web.de>
2017-01-20 21:49:24 +0100

    Add tests for PATCH /api/2/subscriptionEntries

--- myreader-backend/src/test/java/myreader/resource/subscriptionentry/SubscriptionEntryCollectionResourceTest.java
+++ myreader-backend/src/test/java/myreader/resource/subscriptionentry/SubscriptionEntryCollectionResourceTest.java
@@ -1,7 +1,9 @@
 package myreader.resource.subscriptionentry;
 
+import myreader.repository.SubscriptionRepository;
 import myreader.test.IntegrationTestSupport;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static myreader.test.KnownUser.USER107;
 import static myreader.test.KnownUser.USER108;
@@ -13,6 +15,7 @@ import static org.hamcrest.Matchers.endsWith;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.PATCH;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuildersWithAuthenticatedUserSupport.actionAsUserX;
@@ -27,6 +30,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Kamill Sokol
  */
 public class SubscriptionEntryCollectionResourceTest extends IntegrationTestSupport {
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
 
     @Test
     public void testCollectionResourceJsonStructureEquality() throws Exception {
@@ -209,6 +215,10 @@ public class SubscriptionEntryCollectionResourceTest extends IntegrationTestSupp
 
     @Test
     public void testBatchPatch2() throws Exception {
+        //TODO Migrate to WebMvcTest
+        mockMvc.perform(actionAsUserX(PATCH, USER107, "/api/2/subscriptionEntries")
+                .json("{ 'content': [{ 'uuid': '1016', 'seen': true }, { 'uuid': '1017', 'seen': false }]}"));
+
         mockMvc.perform(actionAsUserX(GET, USER107, "/api/2/subscriptionEntries"))
                 .andExpect(jsonEquals("json/subscriptionentry/user107-subscriptionEntries.json"));
 
@@ -248,5 +258,39 @@ public class SubscriptionEntryCollectionResourceTest extends IntegrationTestSupp
     public void availableTags() throws Exception {
         mockMvc.perform(getAsUser4("/api/2/subscriptionEntries/availableTags"))
         .andExpect(jsonPath("$", hasItems("tag1", "tag2-tag3", "tag4", "tag5", "tag6", "tag7", "tag8Tag9")));
+    }
+
+    @Test
+    public void shouldIncrementUnseenCount() throws Exception {
+        assertThat(subscriptionRepository.findOne(105L).getUnseen(), is(1));
+
+        mockMvc.perform(actionAsUserX(PATCH, USER107, "/api/2/subscriptionEntries")
+                .json("{ 'content': [{ 'uuid': '1016', 'seen': false }, { 'uuid': '1017', 'seen': 'false' }]}"));
+
+        assertThat(subscriptionRepository.findOne(105L).getUnseen(), is(2));
+    }
+
+    @Test
+    public void shouldDecrementUnseenCount() throws Exception {
+        //TODO Migrate to WebMvcTest
+        mockMvc.perform(actionAsUserX(PATCH, USER107, "/api/2/subscriptionEntries")
+                .json("{ 'content': [{ 'uuid': '1016', 'seen': false }, { 'uuid': '1017', 'seen': 'true' }]}"));
+
+        int actualCount = subscriptionRepository.findOne(105L).getUnseen();
+
+        mockMvc.perform(actionAsUserX(PATCH, USER107, "/api/2/subscriptionEntries")
+                .json("{ 'content': [{ 'uuid': '1016', 'seen': true }, { 'uuid': '1017', 'seen': 'true' }]}"));
+
+        assertThat(subscriptionRepository.findOne(105L).getUnseen(), is(actualCount - 1));
+    }
+
+    @Test
+    public void shouldNotChangeUnseenCount() throws Exception {
+        int actualCount = subscriptionRepository.findOne(105L).getUnseen();
+
+        mockMvc.perform(actionAsUserX(PATCH, USER107, "/api/2/subscriptionEntries")
+                .json("{ 'content': [{ 'uuid': '1016' }, { 'uuid': '1017' }]}"));
+
+        assertThat(subscriptionRepository.findOne(105L).getUnseen(), is(actualCount));
     }
 }
