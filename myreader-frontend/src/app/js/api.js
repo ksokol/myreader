a9c112e4ccc20ae8815a6f479dd413bb9d4137a4
Kamill Sokol <dev@sokol-web.de>
2017-04-29 15:12:23 +0200

    Restore probeFeedsConverter used in admin ui

--- myreader-frontend/src/app/js/api.js
+++ myreader-frontend/src/app/js/api.js
@@ -26,6 +26,33 @@ angular.module('common.api', [])
     }
 })
 
+.service('probeFeedsConverter', function() {
+
+    var Feeds = function(feeds, links) {
+        var self = this;
+        self.feeds = angular.isArray(feeds) ? feeds : [];
+        self.links = angular.isArray(links) ? links : [];
+
+        var getLink = function(rel) {
+            for(var i=0;i<links.length;i++) {
+                if(links[i].rel === rel) {
+                    return links[i].href;
+                }
+            }
+        };
+
+        self.next = function() {
+            return getLink('next');
+        };
+    };
+
+    return {
+        convertFrom: function (data) {
+            return new Feeds(data.content, data.links);
+        }
+    }
+})
+
 .service('bookmarkTagsConverter', function () {
     return {
         convertFrom: function (data) {
