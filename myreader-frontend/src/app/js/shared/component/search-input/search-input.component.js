6914f16a16c2e08ff1bf3b3fb9cd472589b2df8e
Kamill Sokol <dev@sokol-web.de>
2017-05-26 20:48:31 +0200

    Introduced search-input component.

---
+++ myreader-frontend/src/app/js/shared/component/search-input/search-input.component.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+
+    function SearchInputComponent() {
+        var ctrl = this;
+
+        ctrl.$onInit = function () {
+            ctrl.value = ctrl.myValue || '';
+        };
+
+        ctrl.$onChanges = function (obj) {
+            ctrl.value = obj.myValue.currentValue || '';
+        };
+
+        ctrl.isEmpty = function () {
+            return ctrl.value.length === 0;
+        };
+
+        ctrl.onChange = function () {
+            if (ctrl.isEmpty()) {
+                ctrl.myOnClear();
+            } else {
+                ctrl.myOnChange({ value: ctrl.value });
+            }
+        };
+
+        ctrl.onClear = function () {
+            ctrl.value = '';
+            ctrl.myOnClear();
+        };
+
+        ctrl.css = require('./search-input.component.css');
+    }
+
+    require('angular').module('myreader').component('mySearchInput', {
+        template: require('./search-input.component.html'),
+        controller: SearchInputComponent,
+        bindings: {
+            myValue: '<',
+            myOnChange: '&',
+            myOnClear: '&'
+        }
+    });
+
+    module.exports = 'myreader.search-input.component';
+
+})();
