a63db1a6bc868f6c4782b56f46593d705f3009d1
Kamill Sokol <dev@sokol-web.de>
2017-06-05 11:48:16 +0200

    Fetch all exclusions on subscription page.

--- myreader-frontend/src/app/js/subscription/subscription-exclusion-panel/subscription-exclusion/exclusion.service.js
+++ myreader-frontend/src/app/js/subscription/subscription-exclusion-panel/subscription-exclusion/exclusion.service.js
@@ -4,11 +4,32 @@
     require('angular').module('myreader').service('exclusionService', ['$http', function($http) {
         var url = '/myreader/api/2/exclusions';
 
+        var extractNextHref = function (links) {
+            return links.filter(function (link) { return link.rel === 'next'; })[0];
+        };
+
+        var findBy = function (next) {
+            return $http.get('/myreader' + next.href)
+                .then(function (response) {
+                    return response;
+                });
+        };
+
         return {
             find: function(uuid) {
-                return $http.get(url + '/' + uuid + '/pattern')
-                    .then(function (response) {
-                        return response.data.content;
+                return findBy({ href: '/api/2/exclusions/' + uuid + '/pattern'})
+                    .then(function collect(response) {
+                        var next = extractNextHref(response.data.links);
+
+                        if(!next) {
+                            return response.data.content;
+                        }
+
+                        return findBy(next)
+                            .then(collect)
+                            .then(function (inner) {
+                                return response.data.content.concat(inner);
+                            });
                     });
             },
             save: function(uuid, exclusion) {
