6c0b96cad88b0074c63d429593f3efb2807ad93e
Kamill Sokol <dev@sokol-web.de>
2017-11-18 22:57:45 +0100

    Add test to ScrollIntoViewDirective.

--- myreader-frontend/src/app/js/shared/directive/scroll-into-view/scroll-into-view.directive-spec.js
+++ myreader-frontend/src/app/js/shared/directive/scroll-into-view/scroll-into-view.directive-spec.js
@@ -2,16 +2,23 @@ import {ScrollIntoViewDirective} from "./scroll-into-view.directive";
 
 describe('myreader-frontend/src/app/js/shared/directive/scroll-into-view/scroll-into-view.directive-spec.js', () => {
 
-    let scope, element, attrs, watcher;
+    let scope, element, attrs, watcherExp, watcher;
 
     beforeEach(() => {
         scope = {
-            $watch: (watchExp, listener) => watcher = listener
+            $watch: (watchExp, listener) => {
+                watcherExp = watchExp;
+                watcher = listener;
+            }
         };
         element = [jasmine.createSpyObj('$element', ['scrollIntoView'])];
-        attrs = {myScrollIntoView: null};
+        attrs = {myScrollIntoView: 'expected watcher expression'};
 
         ScrollIntoViewDirective().link(scope, element, attrs);
+    });
+
+    it('should register watch expression', () => {
+        expect(watcherExp).toEqual('expected watcher expression');
     });
 
     it('should scroll element into view when watcher expression becomes true', () => {
