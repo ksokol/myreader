e4001f0c2a628ef05b4f34731e9d8ca5d87c02cf
Kamill Sokol <dev@sokol-web.de>
2017-12-05 19:26:00 +0100

    Extract read/write to/from local storage into separate file.

--- myreader-frontend/src/app/js/store/settings/settings.js
+++ myreader-frontend/src/app/js/store/settings/settings.js
@@ -1,47 +1,32 @@
 import {isBoolean} from '../../shared/utils';
-import {STORAGE_SETTINGS_KEY} from "../../constants";
+import {STORAGE_SETTINGS_KEY} from '../../constants';
+import {readFromStorage, writeToStorage} from '../shared/storage-util';
+
+const key = STORAGE_SETTINGS_KEY;
 
 export function getPageSize() {
-    const {pageSize} = readFromStorage();
+    const {pageSize} = readFromStorage(key);
     return (pageSize > 0 && pageSize <= 30) ? pageSize : 10;
 }
 
 export function isShowUnseenEntries() {
-    const {showUnseenEntries} = readFromStorage();
+    const {showUnseenEntries} = readFromStorage(key);
     return isBoolean(showUnseenEntries) ? showUnseenEntries : true;
 }
 
 export function isShowEntryDetails() {
-    const {showEntryDetails} = readFromStorage();
+    const {showEntryDetails} = readFromStorage(key);
     return isBoolean(showEntryDetails) ? showEntryDetails : true;
 }
 
 export function setPageSize(value) {
-    writeToStorage({...readFromStorage(), pageSize: (value > 0 && value <= 30) ? value : 10});
+    writeToStorage(key, {...readFromStorage(key), pageSize: (value > 0 && value <= 30) ? value : 10});
 }
 
 export function setShowEntryDetails(value) {
-    writeToStorage({...readFromStorage(), showEntryDetails: isBoolean(value) ? value : true});
+    writeToStorage(key, {...readFromStorage(key), showEntryDetails: isBoolean(value) ? value : true});
 }
 
 export function setShowUnseenEntries(value) {
-    writeToStorage({...readFromStorage(), showUnseenEntries: isBoolean(value) ? value : true});
-}
-
-const storage = localStorage;
-
-function readFromStorage() {
-    let source = {};
-    try {
-        source = JSON.parse(storage.getItem(STORAGE_SETTINGS_KEY)) || {};
-    } catch (e) {}
-    return source;
-}
-
-function writeToStorage(object) {
-    storage.setItem(STORAGE_SETTINGS_KEY, toJson(object));
-}
-
-function toJson(object) {
-    return JSON.stringify(object);
+    writeToStorage(key, {...readFromStorage(key), showUnseenEntries: isBoolean(value) ? value : true});
 }
---
+++ myreader-frontend/src/app/js/store/shared/storage-util.js
@@ -0,0 +1,17 @@
+const storage = localStorage;
+
+export function readFromStorage(key) {
+    let source = {};
+    try {
+        source = JSON.parse(storage.getItem(key)) || {};
+    } catch (e) {}
+    return source;
+}
+
+export function writeToStorage(key, object) {
+    storage.setItem(key, toJson(object));
+}
+
+function toJson(object) {
+    return JSON.stringify(object);
+}
