0f22bb6b986fb1a511ae93d78e7c78cfe9a5a5d3
Kamill Sokol <dev@sokol-web.de>
2017-12-09 22:54:51 +0100

    Export common initial state.

--- myreader-frontend/src/app/js/store/common/index.js
+++ myreader-frontend/src/app/js/store/common/index.js
@@ -1,3 +1,12 @@
-export {removeNotification, showSuccessNotification, showErrorNotification} from './actions';
-export {getNotifications} from './selectors';
-export {commonReducers} from './reducers';
+export {removeNotification, showSuccessNotification, showErrorNotification} from './actions'
+export {getNotifications} from './selectors'
+export {commonReducers} from './reducers'
+
+export const initialState = () => {
+    return {
+        notification: {
+            nextId: 0,
+            notifications: []
+        }
+    }
+}
--- myreader-frontend/src/app/js/store/common/reducers.js
+++ myreader-frontend/src/app/js/store/common/reducers.js
@@ -1,26 +1,20 @@
-import * as types from './action-types';
+import * as types from './action-types'
+import {initialState} from './index'
 
-const initialState = {
-    notification: {
-        nextId: 0,
-        notifications: []
-    }
-};
-
-export function commonReducers(state = initialState, action) {
+export function commonReducers(state = initialState(), action) {
     switch (action.type) {
         case types.SHOW_NOTIFICATION: {
-            const nextId = state.notification.nextId + 1;
-            const notifications = [...state.notification.notifications, action.notification];
-            return {...state, notification: {nextId, notifications}};
+            const nextId = state.notification.nextId + 1
+            const notifications = [...state.notification.notifications, action.notification]
+            return {...state, notification: {nextId, notifications}}
         }
         case types.REMOVE_NOTIFICATION: {
-            const nextId = state.notification.nextId;
-            const notifications = state.notification.notifications.filter(it => it.id !== action.id);
-            return {...state, notification: {nextId, notifications}};
+            const nextId = state.notification.nextId
+            const notifications = state.notification.notifications.filter(it => it.id !== action.id)
+            return {...state, notification: {nextId, notifications}}
         }
         default: {
-            return state;
+            return state
         }
     }
 }
