a9846c3ec0e0554959bd4ae5f673772e0bea1b48
Kamill Sokol <dev@sokol-web.de>
2017-12-20 13:09:42 +0100

    Add a convenient function to retrieve the last dispatched action from ngRedux mock.

--- myreader-frontend/src/app/js/shared/test-utils.js
+++ myreader-frontend/src/app/js/shared/test-utils.js
@@ -1,106 +1,107 @@
-'use strict';
+'use strict'
 
 /*
  * https://velesin.io/2016/08/23/unit-testing-angular-1-5-components/
  */
 function componentMock(name) {
     function _componentMock($provide) {
-        _componentMock.bindings = {};
+        _componentMock.bindings = {}
 
         $provide.decorator(name + 'Directive', function($delegate) {
-            var component = $delegate[0];
-            component.template = '';
+            var component = $delegate[0]
+            component.template = ''
             component.controller = function () {
-                _componentMock.bindings = this;
-            };
+                _componentMock.bindings = this
+            }
 
-            return $delegate;
-        });
+            return $delegate
+        })
     }
 
-    return _componentMock;
+    return _componentMock
 }
 
 function directiveMock(name) {
     function _directiveMock($provide) {
-        _directiveMock.scope = {};
+        _directiveMock.scope = {}
 
         $provide.decorator(name + 'Directive', function($delegate) {
-            var directive = $delegate[0];
-            directive.template = '';
+            var directive = $delegate[0]
+            directive.template = ''
             directive.controller = function ($scope) {
-                _directiveMock.scope = $scope;
-            };
-            return $delegate;
-        });
+                _directiveMock.scope = $scope
+            }
+            return $delegate
+        })
     }
 
-    return _directiveMock;
+    return _directiveMock
 }
 
 function filterMock(name) {
     function _filterMock($provide) {
-        var filter = jasmine.createSpy(name + 'Filter');
+        var filter = jasmine.createSpy(name + 'Filter')
         filter.and.callFake(function (value) {
             if (typeof value === 'object') {
                 // remove Angular specific attributes
-                delete value.$$hashKey;
-                delete value.object;
+                delete value.$$hashKey
+                delete value.object
             }
-            return name + '(' + JSON.stringify(value) + ')';
-        });
-        $provide.value(name + 'Filter', filter);
+            return name + '(' + JSON.stringify(value) + ')'
+        })
+        $provide.value(name + 'Filter', filter)
     }
 
-    return _filterMock;
+    return _filterMock
 }
 
 function mock(name) {
     function _mock($provide) {
-        $provide.value(name, {});
+        $provide.value(name, {})
     }
-    return _mock;
+    return _mock
 }
 
 function ngReduxMock() {
-    let _component = null;
-    const mock = jasmine.createSpyObj('$ngRedux', ['dispatch', 'connect', 'subscribe']);
-    mock.onConnect = {};
+    let _component = null
+    const mock = jasmine.createSpyObj('$ngRedux', ['dispatch', 'connect', 'subscribe'])
+    mock.onConnect = {}
     mock.subscribe.and.returnValue(() => {})
     mock.dispatch.and.returnValue({then: () => {}})
-    mock.stateChange = values => Object.assign(_component, values);
-    mock.thunk = state => {
-        mock.dispatch.calls.mostRecent().args[0](mock.dispatch, () => state);
-    };
+    mock.stateChange = values => Object.assign(_component, values)
+    mock.thunk = (state = {}) => {
+        mock.dispatch.calls.mostRecent().args[0](mock.dispatch, () => state)
+    }
     mock.connect.and.callFake((mapStateToTarget, mapDispatchToTarget) => {
         return component => {
-            _component = component;
-            Object.assign(component, mock.onConnect);
-            Object.assign(component, mapStateToTarget);
+            _component = component
+            Object.assign(component, mock.onConnect)
+            Object.assign(component, mapStateToTarget)
             if (mapDispatchToTarget) {
-                Object.assign(component, mapDispatchToTarget(mock.dispatch));
+                Object.assign(component, mapDispatchToTarget(mock.dispatch))
             }
-            return () => {};
+            return () => {}
         }
-    });
-    return mock;
+    })
+    mock.lastAction = () => mock.dispatch.calls.mostRecent().args[0]
+    return mock
 }
 
 function mockNgRedux() {
     function _mock($provide) {
-        $provide.value('$ngRedux', ngReduxMock());
+        $provide.value('$ngRedux', ngReduxMock())
     }
-    return _mock;
+    return _mock
 }
 
 function spy(name) {
     function _spy($provide) {
         $provide.decorator(name, ['$delegate', function ($delegate) {
-                return jasmine.createSpy($delegate);
+                return jasmine.createSpy($delegate)
             }
-        ]);
+        ])
     }
-    return _spy;
+    return _spy
 }
 
 module.exports = {
@@ -111,4 +112,4 @@ module.exports = {
     'spy': spy,
     'mockNgRedux': mockNgRedux,
     'ngReduxMock': ngReduxMock
-};
+}
