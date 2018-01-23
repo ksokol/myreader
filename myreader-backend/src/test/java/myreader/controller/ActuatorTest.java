a34978076b9ae7b5b8093e33221080d89861d0ea
Kamill Sokol <dev@sokol-web.de>
2018-01-23 18:06:44 +0100

    Expose fetchError retainDays configuration value via Spring Boot's application information actuator endpoint.

--- myreader-backend/src/test/java/myreader/controller/ActuatorTest.java
+++ myreader-backend/src/test/java/myreader/controller/ActuatorTest.java
@@ -1,17 +1,20 @@
 package myreader.controller;
 
 import myreader.test.annotation.WithMockUser1;
+import myreader.test.annotation.WithMockUserAdmin;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 /**
@@ -25,10 +28,20 @@ public class ActuatorTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Value("${job.fetchError.retainInDays}")
+    private int retainInDays;
+
     @Test
     @WithMockUser1
     public void shouldReturnGitCommitDetails() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/info"))
+        mockMvc.perform(get("/info"))
                 .andExpect(jsonPath("git", not(empty())));
+    }
+
+    @Test
+    @WithMockUserAdmin
+    public void shouldReturnGitCommitDetails2() throws Exception {
+        mockMvc.perform(get("/info"))
+                .andExpect(jsonPath("fetch-error.retain-days", is(retainInDays)));
     }
 }
