1e86636c548c8a34977cf6b7fe31dbf51e07d1ae
Kamill Sokol <dev@sokol-web.de>
2018-02-14 20:09:58 +0100

    Fix cloneObject function. Function should deep clone array properties.

--- myreader-frontend/src/app/js/store/shared/objects.js
+++ myreader-frontend/src/app/js/store/shared/objects.js
@@ -1,7 +1,7 @@
 export function cloneObject(object) {
     return object ? Object.entries(object).reduce((acc, [key, value]) => {
         if (Array.isArray(value)) {
-            acc[key] = [...value]
+            acc[key] = value.map(cloneObject)
         } else if (typeof value === 'object') {
             acc[key] = cloneObject(value)
         } else {
--- myreader-frontend/src/app/js/store/shared/objects.spec.js
+++ myreader-frontend/src/app/js/store/shared/objects.spec.js
@@ -5,16 +5,16 @@ describe('src/app/js/store/shared/objects.spec.js', () => {
     describe('removeProperties', () => {
 
         it('should clone object', () => {
-            const object = {a: 'b', 'c': 'd'}
+            const object = {a: 'b', c: 'd'}
             const clone = cloneObject(object)
             clone.a = 'e'
             clone.c = 'f'
 
-            expect(object).toEqual({a: 'b', 'c': 'd'})
+            expect(object).toEqual({a: 'b', c: 'd'})
         })
 
         it('should deep clone object with properties', () => {
-            const object = {a: 'b', 'c': {d: 'e'}}
+            const object = {a: 'b', c: {d: 'e'}}
             const clone = cloneObject(object)
             clone.c = {f: 'g'}
 
@@ -22,24 +22,24 @@ describe('src/app/js/store/shared/objects.spec.js', () => {
         })
 
         it('should deep clone object with array property consists of primitives', () => {
-            const object = {a: [1, 2], 'c': [3, 4]}
+            const object = {a: [1, 2], c: [3, 4]}
             const clone = cloneObject(object)
             clone.c = [5, 6]
 
-            expect(object).toEqual({a: [1, 2], 'c': [3, 4]})
+            expect(object).toEqual({a: [1, 2], c: [3, 4]})
         })
 
         it('should deep clone object with array property consists of objects', () => {
-            const object = {a: [1, 2], 'c': [{d: {e: 'f'}}]}
+            const object = {a: [1, 2], c: [{d: {e: 'f'}}]}
             const clone = cloneObject(object)
-            clone.c.d = undefined
+            clone.c[0].d.e = undefined
 
-            expect(object).toEqual({a: [1, 2], 'c': [{d: {e: 'f'}}]})
+            expect(object).toEqual({a: [1, 2], c: [{d: {e: 'f'}}]})
         })
 
         it('should clone properties with null and undefined', () => {
-            expect(cloneObject({a: 'b', 'c': null})).toEqual({a: 'b', 'c': null})
-            expect(cloneObject({a: 'b', 'c': undefined})).toEqual({a: 'b', 'c': undefined})
+            expect(cloneObject({a: 'b', c: null})).toEqual({a: 'b', c: null})
+            expect(cloneObject({a: 'b', c: undefined})).toEqual({a: 'b', c: undefined})
         })
 
         it('should return undefined when given value is undefined', () => {
