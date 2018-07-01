c8785fe701d67c3090b0b4d4c4a277534807c202
Kamill Sokol <dev@sokol-web.de>
2018-07-01 07:04:03 +0200

    Make placeholder in input component configurable.

--- myreader-frontend/src/app/js/shared/component/input/input.js
+++ myreader-frontend/src/app/js/shared/component/input/input.js
@@ -4,38 +4,40 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 
 const Input = props => {
-    return (
-        <div className={classNames('my-input', props.className)}>
-            {props.label && <label htmlFor={props.name}>{props.label}</label>}
+  return (
+    <div className={classNames('my-input', props.className)}>
+      {props.label && <label htmlFor={props.name}>{props.label}</label>}
 
-            <input id={props.name}
-                   type="text"
-                   name={props.name}
-                   value={props.value}
-                   autoComplete="off"
-                   disabled={props.disabled}
-                   onChange={event => props.onChange(event.target.value)} />
+      <input id={props.name}
+             type="text"
+             name={props.name}
+             value={props.value}
+             placeholder={props.placeholder}
+             autoComplete="off"
+             disabled={props.disabled}
+             onChange={event => props.onChange(event.target.value)}/>
 
-            {props.renderValidations()}
-        </div>
-    )
+      {props.renderValidations()}
+    </div>
+  )
 }
 
 Input.propTypes = {
-    className: PropTypes.string,
-    label: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    value: PropTypes.string,
-    disabled: PropTypes.bool,
-    onChange: PropTypes.func,
-    renderValidations: PropTypes.func
+  className: PropTypes.string,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  disabled: PropTypes.bool,
+  onChange: PropTypes.func,
+  renderValidations: PropTypes.func
 }
 
 Input.defaultProps = {
-    className: '',
-    disabled: false,
-    onChange: () => {},
-    renderValidations: () => {}
+  className: '',
+  disabled: false,
+  onChange: () => {},
+  renderValidations: () => {}
 }
 
 export default Input
