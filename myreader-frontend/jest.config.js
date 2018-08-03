d6f3519c50b3c9fb644ee0db5e6521c2629666fa
Kamill Sokol <dev@sokol-web.de>
2018-08-03 20:46:49 +0200

    Update jest 22.4.3 => 23.4.2, jest-css-modules-transform 1.1.0 => 2.0.0

--- myreader-frontend/jest.config.js
+++ myreader-frontend/jest.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '.+\\.css$': '<rootDir>/node_modules/jest-css-modules-transform',
-        '^.+\\.html$': 'html-loader-jest'
-    },
-    collectCoverageFrom: [
-        'src/app/js/**/*.js'
-    ],
-    setupTestFrameworkScriptFile: '<rootDir>/src/tests.js',
-    coverageDirectory: '<rootDir>/build/reports/coverage',
-    testResultsProcessor: 'jest-sonar-reporter',
-    /*
-     * TODO Comes into effect as soon as patched setTimeout function in app.module.js has been removed.
-     */
-    timers: 'fake'
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '.+\\.css$': '<rootDir>/node_modules/jest-css-modules-transform',
+    '^.+\\.html$': 'html-loader-jest'
+  },
+  collectCoverageFrom: [
+    'src/app/js/**/*.js'
+  ],
+  setupTestFrameworkScriptFile: '<rootDir>/src/tests.js',
+  coverageDirectory: '<rootDir>/build/reports/coverage',
+  testResultsProcessor: 'jest-sonar-reporter',
+  /*
+   * TODO Comes into effect as soon as patched setTimeout function in app.module.js has been removed.
+   */
+  timers: 'fake',
+  testURL: 'http://localhost:8080'
 }
