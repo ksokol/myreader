c38b18c8e385df42f32c85666924a9f60b3f529a
Kamill Sokol <dev@sokol-web.de>
2018-08-19 12:04:59 +0200

    Service worker: remove index.html from fallback url.

--- myreader-frontend/webpack.config.js
+++ myreader-frontend/webpack.config.js
@@ -160,7 +160,7 @@ module.exports = function makeWebpackConfig() {
       new SWPrecacheWebpackPlugin({
         filename: 'service-worker.js',
         minify: true,
-        navigateFallback: PUBLIC_URL + '/index.html'
+        navigateFallback: PUBLIC_URL
       }),
 
       new WebpackPwaManifest({
