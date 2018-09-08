ba358a39a347a788b18f93dc8a1b0c42ca2efc92
Kamill Sokol <dev@sokol-web.de>
2018-09-08 21:07:08 +0200

    Fix class name clash in new and old icon button component.

--- myreader-frontend/src/app/js/shared/component/buttons/icon-button/icon-button.component.js
+++ myreader-frontend/src/app/js/shared/component/buttons/icon-button/icon-button.component.js
@@ -1,23 +1,26 @@
 import template from './icon-button.component.html'
 import './icon-button.component.css'
 
+/**
+ * @deprecated
+ */
 class controller {
 
-    get props() {
-        return {
-            type: this.myType,
-            color: this.myColor
-        }
+  get props() {
+    return {
+      type: this.myType,
+      color: this.myColor
     }
+  }
 }
 
 /**
  * @deprecated
  */
 export const IconButtonComponent = {
-    template, controller,
-    bindings: {
-        myType: '@',
-        myColor: '@'
-    }
+  template, controller,
+  bindings: {
+    myType: '@',
+    myColor: '@'
+  }
 }
