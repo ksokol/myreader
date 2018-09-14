0f908dbed128c239a1b7f4e0502ad734765e05e9
Kamill Sokol <dev@sokol-web.de>
2018-09-14 19:31:41 +0200

    Migrate Icon tests to Enzyme.

--- myreader-frontend/src/app/js/components/Icon/Icon.spec.js
+++ myreader-frontend/src/app/js/components/Icon/Icon.spec.js
@@ -1,21 +1,22 @@
 import React from 'react'
-import {shallowOutput} from '../../shared/test-utils'
 import Icon from './Icon'
+import {shallow} from 'enzyme'
 
 describe('src/app/js/components/Icon/Icon.spec.js', () => {
 
   it('should render close icon with default color', () => {
-    expect(shallowOutput(<Icon type="close"/>))
-      .toEqual(<span className="my-icon my-icon__close my-icon--grey"/>)
+    expect(shallow(<Icon type="close"/>).hasClass('my-icon my-icon__close my-icon--grey')).toEqual(true)
   })
 
   it('should render close icon with white color', () => {
-    expect(shallowOutput(<Icon type="close" color="white"/>))
-      .toEqual(<span className="my-icon my-icon__close my-icon--white"/>)
+    expect(shallow(<Icon type="close" color="white"/>).hasClass('my-icon my-icon__close my-icon--white')).toEqual(true)
+  })
+
+  it('should render some other icon', () => {
+    expect(shallow(<Icon type="some" />).hasClass('my-icon__some')).toEqual(true)
   })
 
   it('should disabled icon', () => {
-    expect(shallowOutput(<Icon type="close" disabled={true}/>))
-      .toEqual(<span className="my-icon my-icon__close my-icon--grey my-icon--disabled"/>)
+    expect(shallow(<Icon type="close" disabled={true}/>).hasClass('my-icon my-icon__close my-icon--grey my-icon--disabled')).toEqual(true)
   })
 })
