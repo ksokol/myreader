6a67500b4d18856466fc47fd0a15604f9d7cd910
Kamill Sokol <dev@sokol-web.de>
2018-09-28 22:49:58 +0200

    Make background color of badge component configurable.

--- myreader-frontend/src/app/js/components/Badge/Badge.js
+++ myreader-frontend/src/app/js/components/Badge/Badge.js
@@ -2,13 +2,56 @@ import './Badge.css'
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Badge = props =>
-  <div className='my-badge'>
-    <span>{props.text}</span>
-  </div>
+const DEFAULT_COLOR = '#777'
+const colorCache = new Map([[DEFAULT_COLOR, {red: 119, green: 119, blue: 119}]])
+
+function determineRGB(color) {
+  const ctx = document.createElement('canvas').getContext('2d')
+  ctx.fillStyle = color
+  ctx.fillRect(0, 0, 1, 1)
+  const [red, green, blue] = ctx.getImageData(0, 0, 1, 1).data
+  return {red, green, blue}
+}
+
+class Badge extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.badgeRef = React.createRef()
+  }
+
+  componentDidMount() {
+    const color = this.props.color.toLowerCase()
+    let rgb = colorCache.get(color)
+
+    if (!rgb) {
+      rgb = determineRGB(color)
+      colorCache.set(this.props.color, rgb)
+    }
+
+    const badgeRef = this.badgeRef.current
+    badgeRef.style.setProperty("--red", rgb.red)
+    badgeRef.style.setProperty("--green", rgb.green)
+    badgeRef.style.setProperty("--blue", rgb.blue)
+  }
+
+  render() {
+    return (
+      <div className='my-badge' ref={this.badgeRef}>
+        <span>{this.props.text}</span>
+      </div>
+    )
+  }
+}
 
 Badge.propTypes = {
-  text: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  color: PropTypes.string
+}
+
+Badge.defaultProps = {
+  color: DEFAULT_COLOR
 }
 
 export default Badge
--- myreader-frontend/src/app/js/components/Badge/Badge.spec.js
+++ myreader-frontend/src/app/js/components/Badge/Badge.spec.js
@@ -1,10 +1,80 @@
 import {Badge} from '.'
 import React from 'react'
-import {render} from 'enzyme'
+import {render, mount} from 'enzyme'
 
 describe('Badge', () => {
 
-  it('should render Badge with given text', () => {
+  let context2d
+
+  const uniqueColor = () => `${new Date().getTime()}`
+  const createMount = props => mount(<Badge {...props} />)
+  const byCanvas = it => it[0] === 'canvas'
+
+  beforeEach(() => {
+    context2d = {
+      fillRect: jest.fn(),
+      getImageData: jest.fn().mockReturnValue({data: []})
+    }
+
+    const createElement = document.createElement
+
+    document.createElement = jest.fn().mockImplementation(localName => {
+      if (localName !== 'canvas') {
+        return createElement.call(document, localName)
+      }
+
+      const canvasMock = createElement('div')
+      canvasMock.getContext = param => param === '2d' ? context2d : undefined
+      return canvasMock
+    })
+  })
+
+  it('should render component with given text', () => {
     expect(render(<Badge text='sample text' />).text()).toEqual('sample text')
+  })
+
+  it('should initiate canvas with mandatory values in order to determine RGB value from prop "color"', () => {
+    createMount({color: 'yellow'})
+
+    expect(context2d.fillStyle).toEqual('yellow')
+    expect(context2d.fillRect).toHaveBeenCalledWith(0, 0, 1, 1)
+  })
+
+  it('should transform prop "color" to lower case', () => {
+    createMount({color: '#FFF'})
+
+    expect(context2d.fillStyle).toEqual('#fff')
+  })
+
+  it('should retrieve default RGB value from cache', () => {
+    createMount()
+
+    expect(document.createElement.mock.calls.filter(byCanvas).length).toEqual(0)
+  })
+
+  it('should return cached RGB value when cache contains value for prop "color"', () => {
+    const color = uniqueColor()
+    createMount({color})
+    createMount({color})
+
+    expect(document.createElement.mock.calls.filter(byCanvas).length).toEqual(1)
+  })
+
+  it('should determine RGB value when cache does not contain value for prop "color"', () => {
+    createMount({color: uniqueColor()})
+    createMount({color: uniqueColor()})
+
+    expect(document.createElement.mock.calls.filter(byCanvas).length).toEqual(2)
+  })
+
+  it('should set CSS custom properties red, green and blue from canvas image data', () => {
+    context2d.getImageData = jest.fn().mockReturnValue({data: [100, 101, 102]})
+    const wrapper = createMount({color: uniqueColor()})
+    const spy = jest.spyOn(wrapper.instance().badgeRef.current.style, 'setProperty')
+    wrapper.instance().componentDidMount()
+
+    expect(spy).toHaveBeenNthCalledWith(1, '--red', 100)
+    expect(spy).toHaveBeenNthCalledWith(2, '--green', 101)
+    expect(spy).toHaveBeenNthCalledWith(3, '--blue', 102)
   })
 })
