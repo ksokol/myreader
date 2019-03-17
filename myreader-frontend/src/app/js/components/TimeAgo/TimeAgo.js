2f7cb5dabdc77372ddec12c5600e778d9ea37aac
Kamill Sokol <dev@sokol-web.de>
2019-03-17 12:34:22 +0100

    Add prop type validation back to timeago component.

--- myreader-frontend/src/app/js/components/TimeAgo/TimeAgo.js
+++ myreader-frontend/src/app/js/components/TimeAgo/TimeAgo.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import formatTimeAgo from './formatTimeAgo'
 
 const TimeAgo = React.memo(({date}) => formatTimeAgo(date))
+
+TimeAgo.propTypes = {
+  date: PropTypes.string.isRequired
+}
 
 export default TimeAgo
