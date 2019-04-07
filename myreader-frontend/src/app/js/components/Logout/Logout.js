d5b55635f28181e735838e50979c944bfed861fc
Kamill Sokol <dev@sokol-web.de>
2019-04-07 12:32:43 +0200

    Add prop types to logout component.

--- myreader-frontend/src/app/js/components/Logout/Logout.js
+++ myreader-frontend/src/app/js/components/Logout/Logout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import {withRouter} from 'react-router-dom'
 import {loginRoute} from '../../routes'
 
@@ -11,6 +12,12 @@ class Logout extends React.Component {
   render() {
     return null
   }
+}
+
+Logout.propTypes = {
+  history: PropTypes.shape({
+    replace: PropTypes.func.isRequired
+  }).isRequired
 }
 
 export default withRouter(Logout)
