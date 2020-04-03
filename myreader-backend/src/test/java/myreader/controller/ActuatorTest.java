b5b55ef08b7473aeb1891249749349c6a8056824
Kamill Sokol <dev@sokol-web.de>
2020-04-03 20:07:40 +0200

    test: ActuatorTest should only test if actuator info endpoint is available.

--- myreader-backend/src/test/java/myreader/controller/ActuatorTest.java
+++ myreader-backend/src/test/java/myreader/controller/ActuatorTest.java
@@ -1,18 +1,17 @@
 package myreader.controller;
 
-import myreader.test.annotation.WithMockUser1;
+import myreader.test.TestConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.empty;
-import static org.hamcrest.Matchers.not;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author Kamill Sokol
@@ -26,9 +25,9 @@ public class ActuatorTest {
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser1
-    public void shouldReturnGitCommitDetails() throws Exception {
+    @WithMockUser(TestConstants.USER1)
+    public void shouldReturnHttpOkForActuatorInfoEndpoint() throws Exception {
         mockMvc.perform(get("/info"))
-                .andExpect(jsonPath("git", not(empty())));
+                .andExpect(status().isOk());
     }
 }
