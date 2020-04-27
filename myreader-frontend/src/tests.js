fd5aef9914157b09998d4f953cd775b2536f0a44
Kamill Sokol <dev@sokol-web.de>
2020-04-27 19:53:42 +0200

    chore: Remove unused dev dependency relative-time-format.

--- myreader-frontend/src/tests.js
+++ myreader-frontend/src/tests.js
@@ -3,13 +3,8 @@ import 'intersection-observer'
 import {toContainActionData, toContainObject, toEqualActionType} from './app/js/shared/custom-matcher'
 import {configure} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import RelativeTimeFormat from 'relative-time-format'
-import en from 'relative-time-format/locale/en.json'
 
 configure({adapter: new Adapter()})
-
-RelativeTimeFormat.addLocale(en)
-global['Intl'].RelativeTimeFormat = RelativeTimeFormat
 
 afterEach(() => {
   localStorage.clear()
