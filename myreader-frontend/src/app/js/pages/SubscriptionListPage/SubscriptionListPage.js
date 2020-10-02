8c288ea4edc41d752aad05aaa0c2eb1af6285e7f
Kamill Sokol <dev@sokol-web.de>
2020-10-02 20:33:36 +0200

    fix: add search input back to subscription list page.

--- myreader-frontend/src/app/js/pages/SubscriptionListPage/SubscriptionListPage.js
+++ myreader-frontend/src/app/js/pages/SubscriptionListPage/SubscriptionListPage.js
@@ -1,14 +1,76 @@
+import './SubscriptionListPage.css'
 import React from 'react'
-import {SubscriptionList} from '../../components/SubscriptionList/SubscriptionList'
+import {generatePath} from 'react-router'
 import SubscriptionContext from '../../contexts/subscription/SubscriptionContext'
 import {ListLayout} from '../../components/ListLayout/ListLayout'
+import {SearchInput} from '../../components/SearchInput/SearchInput'
+import {IconButton} from '../../components/Buttons'
+import {useHistory, useSearchParams} from '../../hooks/router'
+import {Link} from 'react-router-dom'
+import {SUBSCRIPTION_URL} from '../../constants'
+import TimeAgo from '../../components/TimeAgo/TimeAgo'
+
+function filterSubscriptions({title}, q = '') {
+  return title.toLowerCase().includes(q.toLowerCase())
+}
 
 export const SubscriptionListPage = () => {
+  const searchParams = useSearchParams()
+  const {push, reload} = useHistory()
+
+  const onChange = q => {
+    push({
+      searchParams: {
+        q
+      }
+    })
+  }
+
   return (
     <SubscriptionContext.Consumer>
       {({subscriptions}) => (
         <ListLayout
-          listPanel={<SubscriptionList subscriptions={subscriptions} />}
+          actionPanel={
+            <>
+              <SearchInput
+                onChange={onChange}
+                value={searchParams.q}
+              />
+              <IconButton
+                type='redo'
+                role='refresh'
+                onClick={reload}
+              />
+            </>
+          }
+          listPanel={
+            <div
+              className='my-subscription-list-page'
+            >
+              {subscriptions
+                .filter(subscription => filterSubscriptions(subscription, searchParams.q))
+                .map(subscription => (
+                  <div
+                    key={subscription.uuid}
+                    className='my-subscription-list-page__item flex flex-col'
+                  >
+                    <Link
+                      className='my-subscription-list-page__item-heading no-underline'
+                      title={subscription.title}
+                      to={generatePath(SUBSCRIPTION_URL, {uuid: subscription.uuid})}
+                    >
+                      {subscription.title}
+                    </Link>
+                    <span>
+                      <TimeAgo
+                        date={subscription.createdAt}
+                      />
+                    </span>
+                  </div>
+                ))
+              }
+            </div>
+          }
         />
       )}
     </SubscriptionContext.Consumer>
--- myreader-frontend/src/app/js/pages/SubscriptionListPage/SubscriptionListPage.spec.js
+++ myreader-frontend/src/app/js/pages/SubscriptionListPage/SubscriptionListPage.spec.js
@@ -1,43 +1,140 @@
 import React from 'react'
-import {mount} from 'enzyme'
+import {render, fireEvent, waitFor, screen, act} from '@testing-library/react'
+import {Router} from 'react-router'
+import {createMemoryHistory} from 'history'
 import {SubscriptionListPage} from './SubscriptionListPage'
-import SubscriptionContext from '../../contexts/subscription/SubscriptionContext'
+import {LocationStateProvider} from '../../contexts/locationState/LocationStateProvider'
+import {SubscriptionProvider} from '../../contexts/subscription/SubscriptionProvider'
 
-/* eslint-disable react/prop-types, react/display-name */
-jest.mock('../../components', () => ({
-  SubscriptionList: () => null,
-}))
-
-jest.mock('../../components/ListLayout/ListLayout', () => ({
-  ListLayout: ({listPanel}) => <div>{listPanel}</div>,
-}))
-/* eslint-enable */
+jest.unmock('react-router')
+jest.unmock('react-router-dom')
 
 describe('SubscriptionListPage', () => {
 
-  let props
+  let history
 
-  const createWrapper = () => mount(
-    <SubscriptionContext.Provider value={props}>
-      <SubscriptionListPage {...props} />
-    </SubscriptionContext.Provider>
-  )
+  const renderComponent = async () => {
+    await act(async () => {
+      render(
+        <Router history={history}>
+          <LocationStateProvider>
+            <SubscriptionProvider>
+              <SubscriptionListPage />
+            </SubscriptionProvider>
+          </LocationStateProvider>
+        </Router>
+      )
+    })
+  }
 
   beforeEach(() => {
-    props = {
-      subscriptions: [
-        {uuid: '1', title: 'title1', createdAt: 'createdAt1'},
-        {uuid: '2', title: 'title2', createdAt: 'createdAt2'},
-      ]
-    }
-  })
+    history = createMemoryHistory()
 
-  it('should initialize list panel component with given props', () => {
-    expect(createWrapper().find('SubscriptionList').props()).toEqual({
-      subscriptions: [
+    fetch.jsonResponseOnce({
+      content: [
         {uuid: '1', title: 'title1', createdAt: 'createdAt1'},
         {uuid: '2', title: 'title2', createdAt: 'createdAt2'},
       ]
     })
+  })
+
+  it('should present given subscriptions', async () => {
+    await renderComponent()
+
+    expect(fetch.mostRecent()).toMatchGetRequest({
+      url: 'api/2/subscriptions',
+    })
+    await waitFor(() => {
+      expect(screen.queryByTitle('title1')).toBeInTheDocument()
+      expect(screen.queryByTitle('title2')).toBeInTheDocument()
+    })
+  })
+
+  it('should navigate to subscription', async () => {
+    await renderComponent()
+
+    await waitFor(() => fireEvent.click(screen.queryByTitle('title1')))
+    expect(history.action).toEqual('PUSH')
+    expect(history.location.pathname).toEqual('/app/subscriptions/1')
+
+    await waitFor(() => fireEvent.click(screen.queryByTitle('title2')))
+    expect(history.action).toEqual('PUSH')
+    expect(history.location.pathname).toEqual('/app/subscriptions/2')
+  })
+
+  it('should filter subscriptions by title title1', async () => {
+    await renderComponent()
+
+    await act(async () => fireEvent.change(screen.getByRole('search'), {target: {value: 'title1'}}))
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('title1')).toBeInTheDocument()
+      expect(screen.queryByTitle('title2')).not.toBeInTheDocument()
+    })
+  })
+
+  it('should filter subscriptions by title title2', async () => {
+    await renderComponent()
+
+    await act(async () => fireEvent.change(screen.getByRole('search'), {target: {value: 'title2'}}))
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('title1')).not.toBeInTheDocument()
+      expect(screen.queryByTitle('title2')).toBeInTheDocument()
+    })
+  })
+
+  it('should filter subscriptions by title TITLE1', async () => {
+    await renderComponent()
+
+    await act(async () => fireEvent.change(screen.getByRole('search'), {target: {value: 'TITLE1'}}))
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('title1')).toBeInTheDocument()
+      expect(screen.queryByTitle('title2')).not.toBeInTheDocument()
+    })
+  })
+
+  it('should filter subscriptions by title titl', async () => {
+    await renderComponent()
+
+    await act(async () => fireEvent.change(screen.getByRole('search'), {target: {value: 'titl'}}))
+
+    // TODO Workaround for "Error: Ran 1000000 timers, and there are still more!" when running all tests
+    await waitFor(() => expect(history.location.search).toEqual('?q=titl'))
+
+    expect(screen.queryByTitle('title1')).toBeInTheDocument()
+    expect(screen.queryByTitle('title2')).toBeInTheDocument()
+  })
+
+  it('should filter subscriptions by title other', async () => {
+    await renderComponent()
+
+    await act(async () => fireEvent.change(screen.getByRole('search'), {target: {value: 'other'}}))
+
+    await waitFor(() => {
+      expect(screen.queryByTitle('title1')).not.toBeInTheDocument()
+      expect(screen.queryByTitle('title2')).not.toBeInTheDocument()
+    })
+  })
+
+  it('should reload subscription when refresh icon button clicked', async () => {
+    await renderComponent()
+
+    fetch.jsonResponseOnce({
+      content: [
+        {uuid: '2', title: 'title2', createdAt: 'createdAt2'},
+        {uuid: '3', title: 'title3', createdAt: 'createdAt3'},
+      ],
+    })
+
+    await act(async () => fireEvent.click(screen.getByRole('refresh')))
+
+    expect(fetch.mostRecent()).toMatchGetRequest({
+      url: 'api/2/subscriptions',
+    })
+    expect(screen.queryByTitle('title1')).not.toBeInTheDocument()
+    expect(screen.queryByTitle('title2')).toBeInTheDocument()
+    expect(screen.queryByTitle('title3')).toBeInTheDocument()
   })
 })
