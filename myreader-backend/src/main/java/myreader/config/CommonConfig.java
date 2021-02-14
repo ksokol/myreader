9197feb827b428ac81150a226ec9e5af2a7b2725
Kamill Sokol <dev@sokol-web.de>
2021-02-14 17:37:51 +0100

    chore: remove unused dependency org.springframework.security:spring-security-data

--- myreader-backend/src/main/java/myreader/config/CommonConfig.java
+++ myreader-backend/src/main/java/myreader/config/CommonConfig.java
@@ -5,40 +5,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 
 import javax.annotation.PostConstruct;
 import java.time.Clock;
 
 import static java.util.Objects.requireNonNull;
 
-/**
- * @author Kamill Sokol
- */
 @Configuration
 @EnableScheduling
 public class CommonConfig {
 
-    private final ThreadPoolTaskExecutor executor;
-    private final SimpleApplicationEventMulticaster eventMulticaster;
+  private final ThreadPoolTaskExecutor executor;
+  private final SimpleApplicationEventMulticaster eventMulticaster;
 
-    public CommonConfig(ThreadPoolTaskExecutor executor, SimpleApplicationEventMulticaster eventMulticaster) {
-        this.executor = requireNonNull(executor, "executor is null");
-        this.eventMulticaster = requireNonNull(eventMulticaster, "eventMulticaster is null");
-    }
+  public CommonConfig(ThreadPoolTaskExecutor executor, SimpleApplicationEventMulticaster eventMulticaster) {
+    this.executor = requireNonNull(executor, "executor is null");
+    this.eventMulticaster = requireNonNull(eventMulticaster, "eventMulticaster is null");
+  }
 
-    @PostConstruct
-    void postConstruct() {
-        eventMulticaster.setTaskExecutor(executor);
-    }
+  @PostConstruct
+  void postConstruct() {
+    eventMulticaster.setTaskExecutor(executor);
+  }
 
-    @Bean
-    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
-        return new SecurityEvaluationContextExtension();
-    }
-
-    @Bean
-    public Clock clock() {
-        return Clock.systemUTC();
-    }
+  @Bean
+  public Clock clock() {
+    return Clock.systemUTC();
+  }
 }
