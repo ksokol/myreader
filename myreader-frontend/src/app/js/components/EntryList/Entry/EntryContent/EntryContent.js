d287766a5c73c5b3a49d5c56d4cef4111762227a
Kamill Sokol <dev@sokol-web.de>
2021-02-23 17:35:47 +0100

    refactor: show and hide entry content based on settings in entry content component

--- myreader-frontend/src/app/js/components/EntryList/Entry/EntryContent/EntryContent.js
+++ myreader-frontend/src/app/js/components/EntryList/Entry/EntryContent/EntryContent.js
@@ -1,16 +1,12 @@
 import './EntryContent.css'
 import React from 'react'
 import PropTypes from 'prop-types'
-import {useMediaBreakpoint} from '../../../../contexts/mediaBreakpoint'
 import {useSettings} from '../../../../contexts/settings'
 
 export function EntryContent({content, maybeVisible}) {
-  const {mediaBreakpoint} = useMediaBreakpoint()
   const {showEntryDetails} = useSettings()
 
-  const isVisible = mediaBreakpoint === 'desktop' ? showEntryDetails || maybeVisible : maybeVisible
-
-  return isVisible ? (
+  return showEntryDetails || maybeVisible ? (
     <div
       className='my-entry-content'
       dangerouslySetInnerHTML={{__html: content}}
--- myreader-frontend/src/app/js/components/EntryList/Entry/EntryContent/EntryContent.spec.js
+++ myreader-frontend/src/app/js/components/EntryList/Entry/EntryContent/EntryContent.spec.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import {mount} from 'enzyme'
 import {EntryContent} from './EntryContent'
-import {useMediaBreakpoint} from '../../../../contexts/mediaBreakpoint'
 import {useSettings} from '../../../../contexts/settings'
 
 /* eslint-disable react/prop-types */
-jest.mock('../../../../contexts/mediaBreakpoint', () => ({
-  useMediaBreakpoint: jest.fn()
-}))
-
 jest.mock('../../../../contexts/settings', () => ({
   useSettings: jest.fn()
 }))
@@ -23,7 +18,6 @@ describe('EntryContent', () => {
   const createWrapper = () => mount(<EntryContent {...props} />)
 
   beforeEach(() => {
-    useMediaBreakpoint.mockClear()
     useSettings.mockClear()
 
     props = {
@@ -34,52 +28,19 @@ describe('EntryContent', () => {
 
   it('should render content', () => {
     useSettings.mockReturnValueOnce({showEntryDetails: true})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'desktop'})
 
     expect(createWrapper().text()).toEqual(expectedContent)
   })
 
-  it('should not render content when "showEntryDetails" is set to false', () => {
+  it('should not render content when "showEntryDetails" and "maybeVisible" are set to false', () => {
     useSettings.mockReturnValueOnce({showEntryDetails: false})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'desktop'})
 
     expect(createWrapper().text()).toEqual('')
   })
 
-  it('should not render content on tablet or phone', () => {
-    useSettings.mockReturnValueOnce({showEntryDetails: false})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'tablet'})
-
-    expect(createWrapper().text()).toEqual('')
-
-    useSettings.mockReturnValueOnce({showEntryDetails: true})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'phone'})
-
-    expect(createWrapper().text()).toEqual('')
-  })
-
-  it('should render content on tablet or phone when "maybeVisible" is set tot true', () => {
-    props.maybeVisible = true
-    useSettings.mockReturnValueOnce({showEntryDetails: true})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'tablet'})
-
-    expect(createWrapper().text()).toEqual(expectedContent)
-
-    useSettings.mockReturnValueOnce({showEntryDetails: true})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'phone'})
-
-    expect(createWrapper().text()).toEqual(expectedContent)
-  })
-
-  it('should render content on tablet or phone when "maybeVisible" is set to true and "showEntryDetails" is set to false', () => {
+  it('should render content when "maybeVisible" is set to true and "showEntryDetails" is set to false', () => {
     props.maybeVisible = true
     useSettings.mockReturnValueOnce({showEntryDetails: false})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'tablet'})
-
-    expect(createWrapper().text()).toEqual(expectedContent)
-
-    useSettings.mockReturnValueOnce({showEntryDetails: true})
-    useMediaBreakpoint.mockReturnValueOnce({mediaBreakpoint: 'phone'})
 
     expect(createWrapper().text()).toEqual(expectedContent)
   })
