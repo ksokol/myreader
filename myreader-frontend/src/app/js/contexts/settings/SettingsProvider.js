2d0d5c5196c80c8706b24648b592b77de722b3ba
Kamill Sokol <dev@sokol-web.de>
2021-03-28 12:07:51 +0200

    refactor: modernize settings context provider

--- myreader-frontend/src/app/js/contexts/settings/SettingsProvider.js
+++ myreader-frontend/src/app/js/contexts/settings/SettingsProvider.js
@@ -1,42 +1,35 @@
-import React from 'react'
+import {useState} from 'react'
 import PropTypes from 'prop-types'
 import SettingsContext from './SettingsContext'
 import {setShowEntryDetails, setShowUnseenEntries, settings} from './settings'
 
-export class SettingsProvider extends React.Component {
+export function SettingsProvider({children}) {
+  const [state, setState] = useState(settings())
 
-  static propTypes = {
-    children: PropTypes.any
-  }
+  return (
+    <SettingsContext.Provider
+      value={{
+        ...state,
+        setShowEntryDetails: showEntryDetails => {
+          setShowEntryDetails(showEntryDetails)
+          setState(current => ({
+            ...current,
+            showEntryDetails
+          }))
+        },
+        setShowUnseenEntries: showUnseenEntries => {
+          setShowUnseenEntries(showUnseenEntries)
+          setState(current => ({
+            ...current,
+            showUnseenEntries
+          }))
+        }
+      }}
+    >{children}
+    </SettingsContext.Provider>
+  )
+}
 
-  state = settings()
-
-  setShowEntryDetails = showEntryDetails => {
-    setShowEntryDetails(showEntryDetails)
-    this.setState({
-      showEntryDetails
-    })
-  }
-
-  setShowUnseenEntries = showUnseenEntries => {
-    setShowUnseenEntries(showUnseenEntries)
-    this.setState({
-      showUnseenEntries
-    })
-  }
-
-  render() {
-    const value = {
-      ...this.state,
-      setShowEntryDetails: this.setShowEntryDetails,
-      setShowUnseenEntries: this.setShowUnseenEntries
-    }
-
-    return (
-      <SettingsContext.Provider
-        value={value}
-      >{this.props.children}
-      </SettingsContext.Provider>
-    )
-  }
+SettingsProvider.propTypes = {
+  children: PropTypes.any
 }
