d1d07d382f5b4a7a18e111de7ed1b42e8a2c4e13
Kamill Sokol <dev@sokol-web.de>
2021-09-17 20:19:47 +0200

    chore: remove unnecessary content negotiation configuration

--- myreader-backend/src/main/java/myreader/config/MvcConfig.java
+++ myreader-backend/src/main/java/myreader/config/MvcConfig.java
@@ -4,23 +4,13 @@ import myreader.resource.subscriptionentry.converter.SearchRequestHandlerMethodA
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
 @Configuration
 @EnableTransactionManagement
 public class MvcConfig implements WebMvcConfigurer {
-
-  @Override
-  public void configureContentNegotiation(final ContentNegotiationConfigurer configurer) {
-    configurer
-      .defaultContentType(APPLICATION_JSON)
-      .favorParameter(false);
-  }
 
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
