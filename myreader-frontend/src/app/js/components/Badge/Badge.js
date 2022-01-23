1d498064b6d8758dc5de3f802d4a319ecf040b5d
Kamill Sokol <dev@sokol-web.de>
2022-01-23 18:49:31 +0100

    fix: badge color flickering

--- myreader-frontend/src/app/js/components/Badge/Badge.js
+++ myreader-frontend/src/app/js/components/Badge/Badge.js
@@ -1,29 +1,22 @@
 import './Badge.css'
-import React, {useCallback, useEffect, useRef} from 'react'
+import {useMemo} from 'react'
 import PropTypes from 'prop-types'
 import determineRGB from './determineRGB'
 
 export function Badge({text, color, role}) {
-  const badgeRef = useRef(null)
-
-  const updateBadgeColor = useCallback(() => {
-    const rgb = determineRGB(color)
-    const current = badgeRef.current
-
-    current.style.setProperty('--red', rgb.red)
-    current.style.setProperty('--green', rgb.green)
-    current.style.setProperty('--blue', rgb.blue)
+  const rgb = useMemo(() => {
+    return determineRGB(color)
   }, [color])
-
-  useEffect(() => {
-    updateBadgeColor()
-  }, [color, updateBadgeColor])
 
   return (
     <div
+      style={{
+        '--red': rgb.red,
+        '--green': rgb.green,
+        '--blue': rgb.blue,
+      }}
       className='my-badge'
       role={role || 'badge'}
-      ref={badgeRef}
     >
       <span>{text}</span>
     </div>
