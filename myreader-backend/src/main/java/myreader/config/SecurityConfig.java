36a3abaa2f3bef5f9a284d538d26af1d0fa6a293
Kamill Sokol <dev@sokol-web.de>
2023-12-25 11:57:39 +0100

    feat: add PWA Manifest

--- myreader-backend/src/main/java/myreader/config/SecurityConfig.java
+++ myreader-backend/src/main/java/myreader/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
   @Bean
   public WebSecurityCustomizer webSecurityCustomizer() {
     return (web) -> web.ignoring()
-      .requestMatchers("/", "/index.html", "/app/**", "/favicon.ico", "/favicon.png");
+      .requestMatchers("/", "/index.html", "/static/**", "/app/**", "/favicon.ico");
   }
 
   @Bean
